7713ce2e323f91d2ff0fb71f083088d02c3851af
saeedo201 <zoalktoom201@gmail.com>
2020-02-13 10:14:47 +0200

    First commit

---
+++ .engine.py
@@ -0,0 +1,149 @@
+#! /bin/env python
+#- coding: utf-8 -#
+#- Import Libraries -#
+import os
+import sys
+from  search_engine_parser import YahooSearch, GoogleSearch, BingSearch, AskSearch, YandexSearch
+import pprint
+import time
+import socket
+os.system('clear')
+#- check internet Connection -#
+def connect():                                     
+   try:                                           
+      ip = socket.gethostbyname("www.google.com") 
+      con = socket.create_connection((ip, 80), 2) 
+      return True                                 
+   except socket.error:                           
+         pass                                     
+   return False                                   
+if connect() !=True:
+        os.system('printf "\e[1;31m"')
+        print("Error: Please Check Your Intenrnet Connection ")
+        exit(1)
+from optparse import OptionParser
+optparse = OptionParser("""
+this script was written by Saeed Ahmeed
+https://www.youtube.com/user/zoalktoom201
+https://twitter.com/RagheZoal
+type python engine.py -h for more help
+engine.py [Options]
+-d / --domin :: domin
+-e / --engine :: engine
+example:
+python engine.py -d sudani.sd -e yahoo
+python engine.py --domim zain.sd --engine google
+python engine.py -d mtn.play.sd -e all
+""")
+optparse.add_option("-d","--domin",dest="domin",type="string",help="Domain to search or company name")
+optparse.add_option("-e","--engine",dest="engine",type="string",help="Engine: google, yahoo, baidu, aol, bing, ask, all")
+(option,args) = optparse.parse_args()
+if option.domin == None and option.engine == None:
+	print(optparse.usage)
+	exit(0)
+elif option.engine not in ("google", "yahoo", "bing","ask","yandex","all"): 
+	os.system('printf "\e[1;31m"')
+	print ("Invalid search engine, try with: google, yahoo, bing, ask, yandex, all")
+	exit(0)
+else:
+	if option.engine == "google":
+		print ("[-] Searching in Google:")
+		gsearch = GoogleSearch()
+		gresults = gsearch.search(option.domin ,1)
+		a = {
+			"Google": gresults}
+		for k, v in a.items():
+			os.system('printf "\e[1;35m"')
+			print(f"-------------{k}------------")
+			#pprint.pprint(a[k]['links'])
+			data_list = (a[k]['links'])
+			data = '\n'.join(data_list)
+			with open("google.txt","a") as f:
+				f.write(f"-------------{k}------------\n") 
+				f.write(data)
+	elif option.engine == "yahoo":	
+		print ("[-] Searching in Yahoo:")
+		yasearch = YahooSearch()
+		yaresults = yasearch.search(option.domin ,1)
+		a = {
+			"Yahoo": yaresults}
+		for k, v in a.items():
+			os.system('printf "\e[1;35m"')
+			print(f"-------------{k}------------")
+			#pprint.pprint(a[k]['links'])
+			data_list = (a[k]['links'])
+			data = '\n'.join(data_list)
+			with open("yahoo.txt","a") as f:
+				f.write(f"-------------{k}------------\n") 
+				f.write(data)				
+	elif option.engine == "bing":	
+		print ("[-] Searching in Bing:")
+		bsearch = BingSearch()
+		bresults = bsearch.search(option.domin ,1)
+		a = {
+			"Bing": bresults}
+		for k, v in a.items():
+			os.system('printf "\e[1;35m"')
+			print(f"-------------{k}------------")
+			#pprint.pprint(a[k]['links'])
+			data_list = (a[k]['links'])
+			data = '\n'.join(data_list)
+			with open("bing.txt","a") as f:
+				f.write(f"-------------{k}------------\n") 
+				f.write(data)
+	elif option.engine == "ask":
+		print ("[-] Searching in Ask:")
+		asearch = AskSearch()
+		aresults = asearch.search(option.domin ,1)
+		a = {
+			"Ask": aresults}
+		for k, v in a.items():
+			os.system('printf "\e[1;35m"')
+			print(f"-------------{k}------------")
+			#pprint.pprint(a[k]['links'])
+			data_list = (a[k]['links'])
+			data = '\n'.join(data_list)
+			with open("ask.txt","a") as f:
+				f.write(f"-------------{k}------------\n") 
+				f.write(data)
+	elif option.engine == "yandex":
+		print ("[-] Searching in Yandex:")
+		ysearch = YandexSearch()
+		yresults = ysearch.search(option.domin ,1)
+		a = {
+			"Yandex": yresults}
+		for k, v in a.items():
+			os.system('printf "\e[1;35m"')
+			print(f"-------------{k}------------")
+			#pprint.pprint(a[k]['links'])
+			data_list = (a[k]['links'])
+			data = '\n'.join(data_list)
+			with open("yandex.txt","a") as f:
+				f.write(f"-------------{k}------------\n") 
+				f.write(data)
+	elif option.engine == "all":
+		os.system('printf "\e[1;34m"')
+		print ("[-] Searching in All Engies:")
+		gsearch = GoogleSearch()
+		yasearch = YahooSearch()
+		bsearch = BingSearch()
+		asearch = AskSearch()
+		gresults = gsearch.search(option.domin ,1)
+		yaresults = yasearch.search(option.domin ,1)
+		bresults = bsearch.search(option.domin ,1)
+		aresults = asearch.search(option.domin ,1)
+		a = { 
+			"Google": gresults,
+			"Yahoo": yaresults,
+			"Bing": bresults,
+			"Ask": aresults}
+			# pretty print the result from each engine
+		for k, v in a.items():
+			os.system('printf "\e[1;40m"')
+			#print(f"\n-------------{k}------------")
+			#pprint.pprint(a[k]['links'])
+			data_list = (a[k]['links'])
+			data = '\n'.join(data_list)
+			with open("all.txt","a") as f:
+				f.write(f"\n-------------{k}------------\n") 
+				f.write(data)
---
+++ start.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+#colors
+g='\033[1;32m'
+p='\033[1;35m'
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+purple='\033[1;35m'
+reset='\033[0m'
+y='\033[1;33m'
+sleep 1
+ clear
+#welcome
+	echo """
+        +=========================================+
+        |..........   Wellcome Guys    ...........|
+        +-----------------------------------------+
+"""
+printf $y
+echo -e " Now You Need Install This Library ::search_engie_parser::"
+sleep 0.5
+printf $reset
+read -p " Do you want to install this library [y/n] : " choice
+if [[ $choice == "y" ]]; then
+clear
+sleep 1
+echo -e "$g+++++++++++++++>$p[Please Wait]$g<+++++++++++++++++"
+pip install search_engine_parser
+sleep 0.9
+clear
+printf "\e[1;45m Well Done!  \e[0m\n"
+printf "\e[1;45m Just Type python engine.py -h for more help \e[0m\n"
+cd $HOME/Engine-Searches && mv .engine.py engine.py && rm start.sh
+fi
+if [[ $choice == "n" ]]; then 
+clear
+printf $red
+echo -e "If You Dont Install This Libarary u cant run this tool"
+fi
